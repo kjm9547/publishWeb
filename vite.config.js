892b00df9c24ae66d94453cf0f2846ad0b947283
JMKIM\jmkim <jmkim2@masangsoft.com>
2024-09-11 17:21:03 +0900

    아이콘 추가

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,46 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/publishweb/',
+  base: '/publishWeb/',
   plugins: [
     react(),
-    VitePWA({registerType:'autoUpdate'})
-  ],
+    VitePWA({
+      registerType:'autoUpdate',
+      manifest:{
+        theme_color: "#4E41DB",
+        background_color: "#4E41DB",
+        display: "standalone",
+        scope: "https://kjm9547.github.io/publishWeb/",
+        start_url: "https://kjm9547.github.io/publishWeb/",
+        name: "Click",
+        short_name: "Ck",
+        
+        icons: [
+          {
+            src: "https://kjm9547.github.io/publishWeb/favicon_32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+            purpose: "any maskable"
+          },
+          {
+            src: "https://kjm9547.github.io/publishWeb/favicon_16x16.png",
+            sizes: "16x16",
+            type: "image/png"
+          },
+          {
+            //스플래시 뷰의 아이콘 역활
+            src: "https://kjm9547.github.io/publishWeb/favicon_192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "https://kjm9547.github.io/publishWeb/favicon_152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+            purpose:"any"
+          },
+        ]
+      }
+    })
+  ]
 })
