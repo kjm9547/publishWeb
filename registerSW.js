d604c9a7c2f52d9f0300b8816f1ece00ff590856
JMKIM\jmkim <jmkim2@masangsoft.com>
2024-09-11 16:04:38 +0900

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/publishweb/sw.js', { scope: '/publishweb/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
